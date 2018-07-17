b24baa03fdb9131799b9bec1b60e673a5a82a810
Daniel Valenzuela <dsvalenzuela@uc.cl>
2018-07-17 05:25:10 -0400

    finished adding styling to the homepage

--- models.py
+++ models.py
@@ -5,3 +5,4 @@ class Project(models.Model):
     name = models.CharField(max_length=100)
     url = models.URLField()
     github = models.URLField()
+    description = models.CharField(max_length=1000)
